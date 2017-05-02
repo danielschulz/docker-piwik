00e8e01ccc5e9d3ed0ff01a023c4ae2d0597d0ab
Daniel Schulz <danielschulz2005@hotmail.com>
2017-05-02 22:59:29 +0200

    added proxy settings added .gitignore file

--- fpm/docker-entrypoint.sh
+++ fpm/docker-entrypoint.sh
@@ -6,4 +6,15 @@ if [ ! -e piwik.php ]; then
 	chown -R www-data .
 fi
 
+
+# add proxy settings
+sed -i -e "s|^assume_secure_protocol \= 0$|assume_secure_protocol = 1|g" /var/www/html/config/global.ini.php
+
+echo -ne "\n" >> /var/www/html/config/global.ini.php
+echo "proxy_client_headers[] = HTTP_X_FORWARDED_FOR" >> /var/www/html/config/global.ini.php
+echo "proxy_host_headers[] = HTTP_X_FORWARDED_HOST" >> /var/www/html/config/global.ini.php
+
+echo -ne "\n" >> /var/www/html/config/global.ini.php
+echo "proxy_ips[] = ${PROXY_IP}" >> /var/www/html/config/global.ini.php
+
 exec "$@"
