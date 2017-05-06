dc60ff084a11a229f3b9bb809dee8178bbf5efbe
Daniel Schulz <danielschulz2005@hotmail.com>
2017-05-06 15:29:13 +0200

    corrected config file patching

--- fpm/docker-entrypoint.sh
+++ fpm/docker-entrypoint.sh
@@ -5,10 +5,12 @@ if [ ! -e piwik.php ]; then
 	tar cf - --one-file-system -C /usr/src/piwik . | tar xf -
 
     # add proxy settings
-    sed -i -e "s|^assume\_secure\_protocol \= 0$|assume_secure_protocol = 1|g" /var/www/html/config/config.ini.php
-    sed -i -e "s|^;proxy\_client\_headers\[\] \= HTTP\_X\_FORWARDED_FOR$|proxy_client_headers[] = HTTP_X_FORWARDED_FOR|g" /var/www/html/config/config.ini.php
-    sed -i -e "s|^;proxy\_host\_headers\[\] \= HTTP\_X\_FORWARDED_HOST$|proxy_host_headers[] = HTTP_X_FORWARDED_HOST|g" /var/www/html/config/config.ini.php
-    sed -i -e "s|^;proxy\_ips\[\] \= 204\.93\.240\.\*$|proxy_ips[] = ${PROXY_IP}|g" /var/www/html/config/config.ini.php
+    export NEW_VALUES="assume_secure_protocol = 1\n"
+    export NEW_VALUES="${NEW_VALUES}proxy_client_headers[] = HTTP_X_FORWARDED_FOR\n"
+    export NEW_VALUES="${NEW_VALUES}proxy_host_headers[] = HTTP_X_FORWARDED_HOST\n"
+    export NEW_VALUES="${NEW_VALUES}proxy_ips[] = ${PROXY_IP}"
+
+    sed -i -e "s|^\[General\]$|[General]\n${NEW_VALUES}|g" /var/www/html/config/config.ini.php
 
 	chown -R www-data .
 fi
