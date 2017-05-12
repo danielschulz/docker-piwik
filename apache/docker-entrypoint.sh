37cc5027d0b28f697d1bc9c3dce7053e45231657
Daniel Schulz <danielschulz2005@hotmail.com>
2017-05-12 15:31:23 +0200

    added LDAP plugin

--- apache/docker-entrypoint.sh
+++ apache/docker-entrypoint.sh
@@ -8,7 +8,8 @@ if [ ! -e piwik.php ]; then
     export NEW_VALUES="assume_secure_protocol = 1\n"
     export NEW_VALUES="${NEW_VALUES}proxy_client_headers[] = HTTP_X_FORWARDED_FOR\n"
     export NEW_VALUES="${NEW_VALUES}proxy_host_headers[] = HTTP_X_FORWARDED_HOST\n"
-    export NEW_VALUES="${NEW_VALUES}proxy_ips[] = ${PROXY_IP}"
+    export NEW_VALUES="${NEW_VALUES}proxy_ips[] = ${PROXY_IP}\n"
+    export NEW_VALUES="${NEW_VALUES}enable_plugin_upload = 1"
 
     sed -i -e "s|^\[General\]$|[General]\n${NEW_VALUES}|g" /var/www/html/config/global.ini.php
 
